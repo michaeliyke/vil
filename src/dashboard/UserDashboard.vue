b27f2fa6b55a19c06061c154d4bfbccdee20e277
michaeliyke <ymichaelc@gmail.com>
2022-04-23 11:35:02 +0100

    preparing for routing

---
+++ src/dashboard/UserDashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="user-dashboard">
+    <h1>User Dashboard</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "UserDashboard",
+};
+</script>
+
+<style>
+</style>
