058647da61404320681c91cc8f5a76d904c8ba1e
michaeliyke <ymichaelc@gmail.com>
2022-04-24 00:24:42 +0100

    Routing - use History mode

---
+++ src/notfound/NotFoundComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="not-found">
+    <h1>This Page could not be found</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NotFoundComponent",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+
+</style>
